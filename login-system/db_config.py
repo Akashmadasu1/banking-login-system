fa287fa664180be6d72ef4565cd3f145e904f211
Akash <akash874426@gmail.com>
2025-06-18 11:38:36 +0530

    Delete login-system directory

--- login-system/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Akashm@123",  # ← Replace this with your MySQL password
-        database="login_system"
-    )
